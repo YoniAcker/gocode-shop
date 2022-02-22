e4485d94b6b34d4e49ddd141c55204943b38be35
יוני אקר <yoniacker@gmail.com>
2022-02-22 23:51:53 +0200

    lesson 2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from './Components/Header/header';
+import Products from './Components/Products/products';
+
+function App() {
+  return (
+    <div>
+    <Header/>
+    <Products/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Products/products.js
@@ -0,0 +1,17 @@
+import Product from'../Product/product';
+import './products.css';
+const Products = () =>(
+    <section className="products">
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+      <Product/>
+    </section>
+  )
+  export default Products;
